0faf7974854e72adfe68233492c45284df761704
Kyle McAdams <kmcadams@concordservicing.com>
2022-10-22 16:12:06 -0700

    adding a querying library, with functionality to find unique items in iterable

---
+++ querying/src/lib.rs
@@ -0,0 +1,99 @@
+// pub fn unique<T: Ord>(mut list: Vec<T>) -> Vec<T> {
+//     list.sort();
+//     list.dedup();
+//     list
+// }
+
+// pub fn extract_unique<T: ?Sized>(list: dyn Iterator<Item=T>) -> Vec<T> {
+//     let mut encountered_items = Vec::new();
+//     list.into_iter().map(|item| if !encountered_items.iter().any(item){encountered_items.push(item);});
+//     encountered_items
+// }
+
+pub fn unique<T:PartialEq, I: IntoIterator<Item=T>>(list: I) -> Vec<T>{
+    let mut encountered: Vec<T> = Vec::new();
+    
+    // // list.into_iter().map(|item| if !encountered.contains(&item){encountered.push(item)});
+    for item in list{
+        if !encountered.contains(&item){
+            encountered.push(item)
+        }
+    }
+    encountered
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ut_empty_list() {
+        let input_list: Vec<i32> = vec![];
+        let expected_output = vec![];
+        assert_eq!(unique(input_list), expected_output);
+    }
+
+    #[test]
+    fn ut_sorted_list_with_duplicates() {
+        let input_vec: Vec<i32> = vec![1,1,2,3,5,5];
+        let expected_ouput = vec![1,2,3,5];        
+        assert_eq!(unique(input_vec), expected_ouput);
+
+        let input_vec: Vec<&str> = vec!["1","1","2","3","5","5"];
+        let expected_output = vec!["1","2","3","5"];
+        assert_eq!(unique(input_vec), expected_output);
+
+        let input_arr = [1,1,2,3,5,5];
+        let expected_ouput = vec![1,2,3,5];
+        assert_eq!(unique(input_arr), expected_ouput);
+
+    }
+
+    #[test]
+    fn ut_sorted_list_with_no_duplicates() {
+        let input_list: Vec<i32> = vec![1,2,3,5];
+        let expected_output = vec![1,2,3,5];
+        assert_eq!(unique(input_list), expected_output);
+
+        let input_list: Vec<&str> = vec!["1","2","3","5"];
+        let expected_output = vec!["1","2","3","5"];
+        assert_eq!(unique(input_list), expected_output);
+
+        let input_arr = [1,2,3,5];
+        let expected_ouput = vec![1,2,3,5];
+        assert_eq!(unique(input_arr), expected_ouput);
+
+    }
+
+    #[test]
+    fn ut_unsorted_list_with_duplicates() {
+        let input_list: Vec<i32> = vec![1,4,4,2,3,1,5];
+        let expected_output = vec![1,4,2,3,5];
+        assert_eq!(unique(input_list), expected_output);
+
+        let input_list: Vec<&str> = vec!["1","4","4","2","3","1","5"];
+        let expected_output = vec!["1","4","2","3","5"];
+        assert_eq!(unique(input_list), expected_output);
+
+        let input_arr = [1,4,4,2,3,1,5];
+        let expected_ouput = vec![1,4,2,3,5];
+        assert_eq!(unique(input_arr), expected_ouput);
+
+    }
+
+    #[test]
+    fn ut_unsorted_list_with_no_duplicates() {
+        let input_list: Vec<i32> = vec![1,5,4,2,3];
+        let expected_output = vec![1,5,4,2,3];
+        assert_eq!(unique(input_list), expected_output);
+
+        let input_list: Vec<&str> = vec!["1","5","4","2","3",];
+        let expected_output = vec!["1","5","4","2","3"];
+        assert_eq!(unique(input_list), expected_output);
+
+        let input_arr = [1,4,2,3,5];
+        let expected_ouput = vec![1,4,2,3,5];
+        assert_eq!(unique(input_arr), expected_ouput);
+    }
+}
